98864d9fadc0e0b650564dfbeef07fc605b421ab
Baha <bakaiks312@gmail.com>
2019-03-18 23:36:05 +0300

    Add class MvcConf in config

--- src/main/java/com/example/Sonoon/config/MvcConf.java
+++ src/main/java/com/example/Sonoon/config/MvcConf.java
@@ -1,4 +1,14 @@
 package com.example.Sonoon.config;
 
-public class MvcConf {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String uploadPath;
+
+
 }

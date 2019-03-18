b1b71cbc8cd36e057791558b0498e6f4281c49e3
Baha <bakaiks312@gmail.com>
2019-03-18 23:32:03 +0300

    Add class Role in domain

--- src/main/java/com/example/Sonoon/domain/Role.java
+++ src/main/java/com/example/Sonoon/domain/Role.java
@@ -1,4 +1,9 @@
 package com.example.Sonoon.domain;
-
-public class Role {
+import org.springframework.security.core.GrantedAuthority;
+public class Role implements GrantedAuthority{
+    USER,EDITOR,MANAGER;
+    @Override
+    public String getAuthority(){
+        return name();
+    }
 }

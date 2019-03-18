62378f6496f545cd4b73c7ff340b008d7b5163bd
Karimov <abdullokh2020@gmail.com>
2019-03-18 17:12:37 +1200

    Класс статьев(article)

--- src/main/java/com/example/Sonoon/domain/Article.java
+++ src/main/java/com/example/Sonoon/domain/Article.java
@@ -1,4 +1,57 @@
 package com.example.Sonoon.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String text;
+    private String zagalovok;
+    private User editor;
+
+
+
+    public Article() {
+    }
+
+    public Article(String text, String zagalovok, User editor) {
+        this.text = text;
+        this.zagalovok = zagalovok;
+        this.editor = editor;
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getZagalovok() {
+        return zagalovok;
+    }
+
+    public void setZagalovok(String zagalovok) {
+        this.zagalovok = zagalovok;
+    }
+
+    public User getEditor() {
+        return editor;
+    }
+
+    public void setEditor(User editor) {
+        this.editor = editor;
+    }
 }

5a516e28a190db0f82fb7ccf2a1c77817b86ef78
Baha <bakaiks312@gmail.com>
2019-03-25 19:38:02 +0300

    Создание модели UserGoogle для UserRepoGoogle

--- src/main/java/com/example/Sonoon/domain/UserGoogle.java
+++ src/main/java/com/example/Sonoon/domain/UserGoogle.java
@@ -1,4 +1,19 @@
 package com.example.Sonoon.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usr")
 public class UserGoogle {
+    @Id
+    private String id;
+    private String name;
+    private String userpic;
+    private String email;
+    private String gender;
+    private String locale;
+    private LocalDateTime lastVisit;
 }

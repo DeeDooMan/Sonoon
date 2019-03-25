0db82d0ed5b9c4a64fe6d45ea697f14b86143f97
Kylym <kylombek@mail.ru>
2019-03-25 17:43:47 +0600

    Added recaptcha backend. Waiting for Front-end.

--- src/main/java/com/example/Sonoon/controller/RegistrationContr.java
+++ src/main/java/com/example/Sonoon/controller/RegistrationContr.java
@@ -2,19 +2,31 @@ package com.example.Sonoon.controller;
 
 import com.example.Sonoon.domain.Role;
 import com.example.Sonoon.domain.User;
+import com.example.Sonoon.domain.dto.CaptchaResponseDto;
 import com.example.Sonoon.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationContr {
+    private static final String CAPTCHA_URL ="https://www.google.com/recaptcha/api/siteverify?secret=%s&response"; //recaptcha post-запрос
+
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${recaptcha.secret}")
+    private String secret; //СЕКРЕТНЫЙ КЛЮЧ РЕКАПЧИ
 
     @GetMapping("/registration")
     public String registration(){
@@ -22,7 +34,17 @@ public class RegistrationContr {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model){
+    public String addUser(
+            @RequestParam("g-recaptcha-response") String recaptchaResponse,
+            User user,
+            Map<String,
+            Object> model){
+        String captchaurl = String.format(CAPTCHA_URL,secret,recaptchaResponse);
+        CaptchaResponseDto captchaResponse = restTemplate.postForObject(captchaurl,Collections.emptyList(), CaptchaResponseDto.class);
+        if(!captchaResponse.isSuccess()){
+            System.out.println("Captcha Error "+ captchaResponse.getErrorCodes());
+        }
+
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
         //Сообщаем если такой пользователь есть в базе данных

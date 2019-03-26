b99a78aa23b3990ee2df9f3657cc463813a1db35
Baha <bakaiks312@gmail.com>
2019-03-26 12:26:13 +0300

    Добавил конфиги для WebSecurityConfigGoogle

--- src/main/java/com/example/Sonoon/config/WebSecurityConfigGoogle.java
+++ src/main/java/com/example/Sonoon/config/WebSecurityConfigGoogle.java
@@ -1,4 +1,33 @@
 package com.example.Sonoon.config;
 
-public class WebSecurityConfigGoogle {
+import com.example.Sonoon.domain.UserGoogle;
+import com.example.Sonoon.repos.UserRepoGoogle;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableOAuth2Sso
+public class WebSecurityConfigGoogle extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PrincipalExtractor principalExtractor(UserRepoGoogle userRepoGoogle){
+        return new UserGoogle();
+    }
 }
+
+

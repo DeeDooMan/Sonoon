6298011891551d3e7ce1d47e43eddf7e8de086ff
KarimovAbdullo <abdullokh2020@gmail.com>
2019-04-29 23:07:42 +0600

    Добавлен тест для UserService addUser()

---
+++ src/test/java/com/example/sonoon/service/UserServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.sonoon.service;
+
+import com.example.sonoon.domain.Role;
+import com.example.sonoon.domain.User;
+import com.example.sonoon.repos.UserRepo;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserSevice userSevice;
+    @MockBean
+    private UserRepo userRepo;
+    @MockBean
+    private MailSender mailSender;
+    @Test
+    public void addUser() {
+        User user = new User();
+
+        user.setEmail("abdullox-karimov@mail.ru");
+
+        boolean isUserCreated = userSevice.addUser(user);
+
+        Assert.assertTrue(isUserCreated);
+        Assert.assertNotNull(user.getActivationCode());
+        Assert.assertTrue(CoreMatchers.is(user.getRoles()).matches(Collections.singleton(Role.USER)));
+
+        Mockito.verify(userRepo, Mockito.times(1)).save(user);
+        Mockito.verify(mailSender, Mockito.times(1))
+                .send(
+                        ArgumentMatchers.eq(user.getEmail()),
+                        ArgumentMatchers.anyString(),
+                        ArgumentMatchers.anyString()
+                );
+    }
+}

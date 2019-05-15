ab98b82dcba4058518fdd8e52ff1c4224ff18fe8
KarimovAbdullo <abdullokh2020@gmail.com>
2019-05-15 23:40:54 +0600

    single mapping

---
+++ src/main/java/com/example/sonoon/controller/SingleController.java
@@ -0,0 +1,74 @@
+package com.example.sonoon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class SingleController {
+    @GetMapping("/single1")
+    public String single1(Map<String, Object> model) {
+        return "single1";
+    }
+    @GetMapping("/single2")
+    public String single2(Map<String, Object> model) {
+        return "single2";
+    }
+    @GetMapping("/single3")
+    public String single3(Map<String, Object> model) {
+        return "single3";
+    }
+    @GetMapping("/single4")
+    public String single4(Map<String, Object> model) {
+        return "single4";
+    }
+    @GetMapping("/single5")
+    public String single5(Map<String, Object> model) {
+        return "single5";
+    }
+    @GetMapping("/single6")
+    public String single6(Map<String, Object> model) {
+        return "single6";
+    }
+    @GetMapping("/single7")
+    public String single7(Map<String, Object> model) {
+        return "single7";
+    }
+    @GetMapping("/single8")
+    public String single8(Map<String, Object> model) {
+        return "single8";
+    }
+    @GetMapping("/single9")
+    public String single9(Map<String, Object> model) {
+        return "single9";
+    }
+    @GetMapping("/single10")
+    public String single10(Map<String, Object> model) {
+        return "single10";
+    }
+    @GetMapping("/single11")
+    public String single11(Map<String, Object> model) {
+        return "single11";
+    }
+    @GetMapping("/single12")
+    public String single12(Map<String, Object> model) {
+        return "single12";
+    }
+    @GetMapping("/single13")
+    public String single13(Map<String, Object> model) {
+        return "single13";
+    }
+    @GetMapping("/single14")
+    public String single14(Map<String, Object> model) {
+        return "single14";
+    }
+    @GetMapping("/single15")
+    public String single15(Map<String, Object> model) {
+        return "single15";
+    }
+    @GetMapping("/single16")
+    public String single16(Map<String, Object> model) {
+        return "single16";
+    }
+}
